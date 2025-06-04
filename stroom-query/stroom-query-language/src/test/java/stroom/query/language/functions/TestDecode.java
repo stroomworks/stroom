f4b0f68f1fdb547af1de71a128b2473299764953
stroomworks1876 <stroomworks1876@gmail.com>
2025-06-04 09:08:47 +0100

    gchq/stroom#4112 Return regex capture groups

--- stroom-query/stroom-query-language/src/test/java/stroom/query/language/functions/TestDecode.java
+++ stroom-query/stroom-query-language/src/test/java/stroom/query/language/functions/TestDecode.java
@@ -28,7 +28,32 @@ public class TestDecode extends AbstractFunctionTest<Decode> {
                         ValString.create("rgb(255,0,0)"),
                         ValString.create("^blue"),
                         ValString.create("rgb(0,0,255)"),
-                        ValString.create("rgb(0,255,0)"))
+                        ValString.create("rgb(0,255,0)")),
+                TestCase.of(
+                        "returnsCaptureGroup",
+                        ValString.create("blue"),
+                        ValString.create("red, white and blue"),
+                        ValString.create("^red, (\\w+) and (\\w+)"),
+                        ValString.create("$2"),
+                        ValString.create("NO MATCH FOUND")),
+                TestCase.of(
+                        "withChildFunctionReturnsCaptureGroup",
+                        ValString.create("white"),
+                        ValString.create("red, white and blue"),
+                        ValString.create("^red, (\\w+) and (\\w+)"),
+                        ValString.create("$1"),
+                        ValString.create("^green"),
+                        new Null("null"),
+                        ValString.create("NO MATCH FOUND")),
+                TestCase.of(
+                        "withChildFunctionReturnsError",
+                        ValErr.create("Unable to get capture group $4 from regex"),
+                        ValString.create("red, white and blue"),
+                        ValString.create("^red, (\\w+) and (\\w+)"),
+                        ValString.create("$4"),
+                        ValString.create("^green"),
+                        new Null("null"),
+                        ValString.create("NO MATCH FOUND"))
         );
     }
 }
