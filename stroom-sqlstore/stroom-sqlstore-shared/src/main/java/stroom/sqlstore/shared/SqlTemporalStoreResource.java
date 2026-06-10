e28da0be24f2937898f05ba4fc11828e69f9b107
Stroomworks 4092 <stroomworks4092@gmail.com>
2026-06-10 13:12:07 +0100

    Add REST API for the updatable temporal store

--- stroom-sqlstore/stroom-sqlstore-shared/src/main/java/stroom/sqlstore/shared/SqlTemporalStoreResource.java
+++ stroom-sqlstore/stroom-sqlstore-shared/src/main/java/stroom/sqlstore/shared/SqlTemporalStoreResource.java
@@ -17,8 +17,12 @@
 package stroom.sqlstore.shared;
 
 import stroom.docref.DocRef;
+import stroom.entity.shared.ExpressionCriteria;
 import stroom.util.shared.ResourcePaths;
 import stroom.util.shared.RestResource;
+import stroom.util.shared.ResultPage;
+import stroom.util.shared.TemporalEntry;
+import stroom.util.shared.TemporalEntryId;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -26,6 +30,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -50,4 +55,34 @@ public interface SqlTemporalStoreResource extends RestResource, DirectRestServic
     @Operation(summary = "Get store count",
             description = "Returns the number of entries in the specified store.")
     Long count(@Parameter(description = "docRef", required = true) DocRef docRef);
+
+    @POST
+    @Path("/entry")
+    @Operation(summary = "Create an entry",
+            description = "Creates a new temporal entry in the store.")
+    TemporalEntry create(@Parameter(description = "entry", required = true) TemporalEntry entry);
+
+    @PUT
+    @Path("/entry")
+    @Operation(summary = "Update an entry",
+            description = "Updates an existing temporal entry in the store.")
+    TemporalEntry update(@Parameter(description = "entry", required = true) TemporalEntry entry);
+
+    @POST
+    @Path("/entry/fetch")
+    @Operation(summary = "Fetch an entry",
+            description = "Fetches a specific entry from the store.")
+    TemporalEntry fetch(@Parameter(description = "id", required = true) TemporalEntryId id);
+
+    @POST
+    @Path("/entry/delete")
+    @Operation(summary = "Delete an entry",
+            description = "Deletes a specific entry from the store.")
+    Boolean delete(@Parameter(description = "id", required = true) TemporalEntryId id);
+
+    @POST
+    @Path("/find")
+    @Operation(summary = "Find entries",
+            description = "Finds entries matching criteria.")
+    ResultPage<TemporalEntry> find(@Parameter(description = "criteria", required = true) ExpressionCriteria criteria);
 }
